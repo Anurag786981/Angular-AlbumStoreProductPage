70a631411f7f90afa81768c20075a6de360f95e8
Anurag <anuragtiwariji2012@gmail.com>
2023-03-23 13:32:12 +0530

    change for module 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
+
+import { ProductService } from './product.service'
 
 @NgModule({
   declarations: [
@@ -13,7 +19,7 @@ import { ProductDescriptionComponent } from './product-description/product-descr
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppM
+export class AppModule { }
